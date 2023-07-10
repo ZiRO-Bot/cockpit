b86f364b51509194ee0c36eb387bba27afb2b1e3
ziro <palembani@gmail.com>
2023-07-10 08:44:16 +0700

    refactor: Start rewrite

--- src/utils/constant.js
+++
@@ -1,3 +0,0 @@
-export const inviteUrl = "https://discord.com/oauth2/authorize?client_id=740122842988937286&scope=bot&permissions=2080898294"
-export const apiUrl = "http://127.0.0.1/api"
-export const loginUrl = apiUrl + "/login"
