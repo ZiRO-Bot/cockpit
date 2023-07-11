0d1bc18da69b8d83bceb547af035620d11e899ff
ziro <palembani@gmail.com>
2023-07-11 14:46:55 +0700

    fix: Spinner color to support dark mode

--- src/widget/spinner.tsx
+++ src/widget/spinner.tsx
@@ -1,12 +1,14 @@
 interface SpinnerAttribute {
     size?: number
     color?: string
+    darkColor?: string
     strokeWidth?: number
     className?: string
 }
 export const Spinner = ({
     size = 48,
-    color = "#FFFFFF",
+    color = "black",
+    darkColor = "white",
     strokeWidth = 2,
     className = "",
 }: SpinnerAttribute) => {
@@ -15,14 +17,12 @@ export const Spinner = ({
             <svg
                 width={size}
                 height={size}
-                color={color}
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className={"animate-spinner overflow-visible " + className}>
+                className={`${className} animate-spinner overflow-visible stroke-${color} dark:stroke-${darkColor}`}>
                 <path
                     d="M23 12C23 18.0751 18.0751 23 12 23"
-                    stroke={color}
                     stroke-width={strokeWidth}
                     stroke-linecap="round"
                 />
