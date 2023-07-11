b95ec8ac81922e8cde2213b32965ba0927bc5cc5
ziro <palembani@gmail.com>
2023-07-11 09:02:46 +0700

    style: Normalize CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app"
-import "/public/globals.css"
+import "/public/normalize.css"
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
--- src/ui/layouts/root.tsx
+++ src/ui/layouts/root.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
 const inter = Inter({
@@ -7,11 +6,6 @@ const inter = Inter({
     display: "swap",
     variable: "--font-inter",
 })
-
-export const metadata: Metadata = {
-    title: "Z3R0",
-    description: "An open-source multi-purpose discord bot",
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return <body className={`${inter.variable} font-sans`}>{children}</body>
