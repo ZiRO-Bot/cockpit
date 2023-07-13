fd707af0be813be0012bbae6e31f0122215df846
ziro <palembani@gmail.com>
2023-07-13 13:58:24 +0700

    feat: Detail content sample

--- src/ui/views/home.tsx
+++ src/ui/views/home.tsx
@@ -29,6 +29,24 @@ const HomeView: NextPage = () => {
                     </Button>
                 </div>
             </div>
+            <div className="flex bg-gray-200/[0.8] dark:bg-gray-50/[0.02]">
+                <div className="mx-5 mt-12 lg:mt-24 mb-20 lg:mb-32 w-full flex flex-col items-center justify-center">
+                    <h1 className="mb-8 text-5xl font-bold">Custom Commands</h1>
+                    <p className="max-w-2xl text-lg lg:text-xl text-center font-medium leading-normal">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Nunc id cursus metus
+                        aliquam eleifend mi in nulla posuere.
+                    </p>
+                    <div className="mt-16 gap-3 w-full sm:w-auto text-center flex flex-col sm:flex-row">
+                        <Button
+                            buttonType={ButtonType.PRIMARY}
+                            href="https://docs.zibot.aap.my.id"
+                            aria-label="Go to documentation">
+                            See documentation
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </LandingLayout>
     )
 }
