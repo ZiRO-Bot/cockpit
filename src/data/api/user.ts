6511a9e7f3f6efd85c214e0ea43b1d4bab9a5fd6
ziro <palembani@gmail.com>
2023-07-14 12:53:24 +0700

    feat: Fetch user

--- src/data/api/user.ts
+++ src/data/api/user.ts
@@ -1,8 +1,8 @@
-export const fetchUser = () => {
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/v2/@me`, {
+import User from "@/model/user"
+
+export const fetchUser = async (): Promise<User> => {
+    const resp = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/v2/@me`, {
         credentials: "include",
-    }).then((resp) => {
-        return resp.text()
     })
-    return ""
+    return await resp.json()
 }
