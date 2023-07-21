7646a5ecf2d8d4d64db7cf006b5c2d9e6e523727
ziro <palembani@gmail.com>
2023-07-21 11:32:36 +0700

    chore: Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,26 @@ import { ReactNode } from "react"
 import "../style/globals.css"
 import "../style/normalize.css"
 
+const title = "Z3R0"
+const description = "An open-source multi-purpose discord bot"
+
 export const metadata: Metadata = {
-    title: "Z3R0",
-    description: "An open-source discord bot",
+    title: title,
+    description: description,
+    openGraph: {
+        type: "website",
+        siteName: title,
+        title: title,
+        description: description,
+        images: [
+            {
+                url: "https://raw.githubusercontent.com/ZiRO-Bot/Z3R0/overhaul/assets/img/banner.png",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+    },
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
